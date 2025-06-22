a19c0e7c91bf6e2911a3467482356f0169ca9643
akshat <akshatsrivastava2905@gmail.com>
2025-06-23 00:38:59 +0530

    Added Happy Number

---
+++ Fast & Slow Pointers/Happy Number.cpp
@@ -0,0 +1,59 @@
+/*
+Write an algorithm to determine if a number n is happy.
+
+A happy number is a number defined by the following process:
+
+Starting with any positive integer, replace the number by the sum of the squares of its digits.
+Repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1.
+Those numbers for which this process ends in 1 are happy.
+Return true if n is a happy number, and false if not.
+
+ 
+
+Example 1:
+
+Input: n = 19
+Output: true
+Explanation:
+12 + 92 = 82
+82 + 22 = 68
+62 + 82 = 100
+12 + 02 + 02 = 1
+Example 2:
+
+Input: n = 2
+Output: false
+ 
+
+Constraints:
+
+1 <= n <= 231 - 1
+*/
+
+class Solution {
+public:
+    int helper(int n ){
+        int temp =0;
+        while(n>0){
+            int a = n%10;
+            cout<<a<<" ";
+            temp+= a*a;
+            n/=10;
+        }
+        cout<<temp<<endl;
+        return temp;
+    }
+    bool isHappy(int n) {
+        set<int>s;
+        s.insert(n);
+        while(true){
+            n = helper( n);
+            if(n==1) return true;
+            if(s.find(n)!=s.end()){
+                return false;
+            }
+            s.insert(n);
+        }
+        return false;
+    }
+};
